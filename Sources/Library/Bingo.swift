dcb948a44a5374c40e2ece405238ff7c0e7f7a0c
Peter VanLund <792688+petermeansrock@users.noreply.github.com>
2021-12-03 22:35:42 -0800

    refactor(day4): clean up solution slightly

--- Sources/Library/Bingo.swift
+++ Sources/Library/Bingo.swift
@@ -1,27 +1,5 @@
 import Foundation
 
-/*
- 7,4,9,5,11,17,23,2,0,14,21,24,10,16,13,6,15,25,12,22,18,20,8,19,3,26,1
-
- 22 13 17 11  0
-  8  2 23  4 24
- 21  9 14 16  7
-  6 10  3 18  5
-  1 12 20 15 19
-
-  3 15  0  2 22
-  9 18 13 17  5
- 19  8  7 25 23
- 20 11 10 24  4
- 14 21 16 12  6
-
- 14 21 17 24  4
- 10 16 15  9 19
- 18  8 23 26 20
- 22 11 13  6  5
-  2  0 12  3  7
- */
-
 public struct Spot {
     public let number: Int
     public var isMarked: Bool
@@ -34,27 +12,20 @@ public struct Spot {
 
 public struct BoardSystem {
     private var boards: [Board]
-    private var sequence: [Int]
+    private let sequence: [Int]
     
     public init(lines: [String]) {
-        var boards = [Board]()
-        var sequence: [Int]? = nil
-        var boardLines: [String]? = nil
-        for line in lines {
-            if sequence == nil {
-                sequence = line.components(separatedBy: ",").map({ Int($0)! })
-            } else if line.count == 0 && boardLines != nil {
-                var newBoard = Board(lines: boardLines!)
-                boards.append(newBoard)
-                boardLines = [String]()
-            } else if line.count == 0 {
-                boardLines = [String]()
+        self.sequence = lines[0].components(separatedBy: ",").map({ Int($0)! })
+        self.boards = []
+        var boardLines: [String] = []
+        for line in lines[2...] {
+            if line.count == 0 {
+                self.boards.append(Board(lines: boardLines))
+                boardLines = []
             } else {
-                boardLines!.append(line)
+                boardLines.append(line)
             }
         }
-        self.sequence = sequence!
-        self.boards = boards
     }
     
     public mutating func play() -> Int {
@@ -115,14 +86,7 @@ public struct Board {
         }
         
         if self.hasWon() {
-            var unmarkedSum = 0
-            for i in 0..<5 {
-                for j in 0..<5 {
-                    if !self.numbers[i][j].isMarked {
-                        unmarkedSum += self.numbers[i][j].number
-                    }
-                }
-            }
+            let unmarkedSum = self.numbers.flatMap{ $0 }.filter{ !$0.isMarked }.map{ $0.number }.reduce(0, +)
             return number * unmarkedSum
         }
         
@@ -131,21 +95,19 @@ public struct Board {
     
     private func hasWon() -> Bool {
         // Horizontal wins
-        for i in 0..<5 {
-            if self.numbers[i].filter({ $0.isMarked }).count == 5 {
+        for row in 0..<5 {
+            if self.numbers[row].filter({ $0.isMarked }).count == 5 {
                 return true
             }
         }
         
         // Vertical wins
-        for i in 0..<5 {
-            if self.numbers.map({ $0[i] }).filter({ $0.isMarked }).count == 5 {
+        for column in 0..<5 {
+            if self.numbers.map({ $0[column] }).filter({ $0.isMarked }).count == 5 {
                 return true
             }
         }
         
         return false
     }
-    
-    
 }
