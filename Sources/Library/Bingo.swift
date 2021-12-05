ef1b3ca9e05dbc0afce4b831c7c268a8f49f81bb
Peter VanLund <792688+petermeansrock@users.noreply.github.com>
2021-12-04 17:00:20 -0800

    docs(day4): document the bingo structures

--- Sources/Library/Bingo.swift
+++ Sources/Library/Bingo.swift
@@ -1,19 +1,43 @@
 import Foundation
 
+/// Represents a single spot on a bingo board which can be marked when a number is selected.
 public struct Spot {
+    /// The number that must be called to mark this spot.
     public let number: Int
+    /// Whether or not this number has been called.
     public var isMarked: Bool
     
+    /// Creates a new instance..
+    ///
+    /// - Parameters:
+    ///   - number: The number of this spot.
+    ///   - isMarked: The initial set of this spot, defaulting to `false`.
     public init(number: Int, isMarked: Bool = false) {
         self.number = number
         self.isMarked = isMarked
     }
 }
 
+/// Represents a bingo board.
 public struct Board {
     private var numbers = [[Spot]]()
     private var winner: Int?
     
+    /// Creates a new instance.
+    ///
+    /// This constructor expects multiple strings, each representing a row of numbers. The numbers
+    /// per row are delimited by spaces (and may be preceded by spaces) as demonstrated in the
+    /// following example:
+    ///
+    /// ```
+    /// 22 13 17 11  0
+    ///  8  2 23  4 24
+    /// 21  9 14 16  7
+    ///  6 10  3 18  5
+    ///  1 12 20 15 19
+    /// ```
+    ///
+    /// - Parameter lines: The Lines representing a board.
     public init(lines: [String]) {
         for line in lines {
             let row = line
@@ -23,6 +47,11 @@ public struct Board {
         }
     }
     
+    /// Plays a number by marking all spots with the associated number.
+    ///
+    /// - Parameter number: The number for which to mark matching spots.
+    /// - Returns: Upon playing a winning number, the winning number multiplied by the sum of all
+    ///   remaining, unmarked numbers. Otherwise, returns an empty optional.
     public mutating func play(number: Int) -> Int? {
         for i in self.numbers.indices {
             for j in self.numbers[i].indices {
@@ -49,19 +78,47 @@ public struct Board {
             || self.numbers.columns().first(where: Board.isWinningSequence) != nil // Vertical
     }
     
-    /// A predicate for evaluating whether or not all spots in the array are marked.
-    ///
-    /// - Parameter spots: The spots to examine.
-    /// - Returns: True if all spots in the array are marked, false otherwise.
     private static func isWinningSequence(in spots: [Spot]) -> Bool {
         return spots.filter{ $0.isMarked }.count == spots.count
     }
 }
 
+/// Represents multiple playable bingo boards as well as the sequence of numbers to play.
 public struct BoardSystem {
     private var boards: [Board]
     private let sequence: [Int]
     
+    /// Creates a new instance.
+    ///
+    /// This constructor expects input composed of two pieces, delimited by an empty line of data:
+    /// 1. A single line of comma-delimited integers representing the sequence of numbers to play.
+    /// 1. One or more board representations, delimited by empty lines of data, each board matching
+    ///    the format expected by `Board`.``Board/init(lines:)``.
+    ///
+    /// As one example:
+    ///
+    /// ```
+    /// 7,4,9,5,11,17,23,2,0,14,21,24,10,16,13,6,15,25,12,22,18,20,8,19,3,26,1
+    ///
+    /// 22 13 17 11  0
+    ///  8  2 23  4 24
+    /// 21  9 14 16  7
+    ///  6 10  3 18  5
+    ///  1 12 20 15 19
+    ///
+    ///  3 15  0  2 22
+    ///  9 18 13 17  5
+    /// 19  8  7 25 23
+    /// 20 11 10 24  4
+    /// 14 21 16 12  6
+    ///
+    /// 14 21 17 24  4
+    /// 10 16 15  9 19
+    /// 18  8 23 26 20
+    /// 22 11 13  6  5
+    ///  2  0 12  3  7
+    /// ```
+    /// - Parameter lines: Lines representing a playable sequence of numbers and multiple boards.
     public init(lines: [String]) {
         self.sequence = lines[0].components(separatedBy: ",").map({ Int($0)! })
         self.boards = []
@@ -76,29 +133,31 @@ public struct BoardSystem {
         }
     }
     
-    public mutating func play() -> Int {
-        for n in sequence {
-            for (i, _) in boards.enumerated() {
-                if let solution = boards[i].play(number: n) {
-                    return solution
-                }
-            }
-        }
-        
-        return -1
+    /// Plays the sequence of numbers until the first board wins.
+    ///
+    /// - Returns: `Board`.``Board/play(number:)`` of the first winnig board.
+    public mutating func play() -> Int? {
+        return self.play(through: 1)
     }
     
-    public mutating func playThroughLastBoard() -> Int {
-        var activeBoards = boards.count
+    /// Plays the sequence of numbers until the last board wins.
+    ///
+    /// - Returns: `Board`.``Board/play(number:)`` of the last winning board.
+    public mutating func playThroughLastBoard() -> Int? {
+        return self.play(through: self.boards.count)
+    }
+    
+    private mutating func play(through winningBoardCount: Int) -> Int? {
+        var remainingBoards = winningBoardCount
         var deadBoards = Array(repeating: false, count: boards.count)
         for n in sequence {
             for (i, _) in boards.enumerated() {
                 if !deadBoards[i] {
                     if let solution = boards[i].play(number: n) {
-                        if activeBoards == 1 {
+                        if remainingBoards == 1 {
                             return solution
                         } else {
-                            activeBoards -= 1
+                            remainingBoards -= 1
                             deadBoards[i] = true
                         }
                     }
@@ -106,6 +165,6 @@ public struct BoardSystem {
             }
         }
         
-        return -1
+        return nil
     }
 }
