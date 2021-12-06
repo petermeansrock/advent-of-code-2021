e876ea8f6bdbc52e29ac87b806babc106f7a61eb
Peter VanLund <792688+petermeansrock@users.noreply.github.com>
2021-12-06 10:21:53 -0800

    perf(day6): forget previous days to reduce memory

--- Sources/Library/Spawner.swift
+++ Sources/Library/Spawner.swift
@@ -7,37 +7,40 @@ public struct Spawner {
         // This is the maximum value we will ever expect for a single spawned entity
         let initialSpawnValue = (cycle - 1) + childhood
         
-        // Prepare a two-dimensional array where the outside array represents each day
-        // and the inner array contains the counts of each spawned entity sharing the
-        // same remaining days to spawn (who will always remain in sync with one another)
-        var spawnGroupsByDay = Array(repeating: Array(repeating: 0, count: initialSpawnValue + 1), count: days + 1)
+        // Prepare blank slate for initializing each day
+        let emptyDay = Array(repeating: 0, count: initialSpawnValue + 1)
         
-        // Initialize day zero by counting the number of spawned entity which share the same number of days remaining
-        spawnGroupsByDay[0] = initialGroups.reduce(into: spawnGroupsByDay[0]) { counts, daysRemaining in
+        // Initialize day zero by counting the number of spawned entities which share the same
+        // number of days remaining
+        var previousDay = initialGroups.reduce(into: emptyDay) { counts, daysRemaining in
             counts[daysRemaining] += 1
         }
     
         // Simulate each day
-        for day in 1...days {
+        for _ in 1...days {
+            // Create blank state for new day
+            var newDay = emptyDay
+            
             // Examine each bucket of spawned entities by the number of days remaining
-            for daysRemaining in spawnGroupsByDay[day].indices {
+            for (daysRemaining, previousCount) in previousDay.enumerated() {
                 // Base the new day's values off the counts from the previous day
-                let previousCount = spawnGroupsByDay[day - 1][daysRemaining]
-                
                 if daysRemaining == 0 {
                     // The group at zero now reset their spawn cycle
-                    spawnGroupsByDay[day][cycle - 1] += previousCount
+                    newDay[cycle - 1] += previousCount
                     
                     // Also, the same number of fish are spawned at the initial spawn value
-                    spawnGroupsByDay[day][initialSpawnValue] += previousCount
+                    newDay[initialSpawnValue] += previousCount
                 } else {
                     // Otherwise, we just shift the previous count down by one day
-                    spawnGroupsByDay[day][daysRemaining - 1] += previousCount
+                    newDay[daysRemaining - 1] += previousCount
                 }
             }
+            
+            // Update previous to contain the new day's values
+            previousDay = newDay
         }
         
         // Calculcate the sum of the final day
-        self.count = spawnGroupsByDay[days].reduce(0, +)
+        self.count = previousDay.reduce(0, +)
     }
 }
