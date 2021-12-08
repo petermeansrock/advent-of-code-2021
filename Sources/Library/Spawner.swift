f7ce014f8ce99161b27ee2a098c09db98279f0a8
Peter VanLund <792688+petermeansrock@users.noreply.github.com>
2021-12-07 20:07:32 -0800

    docs(day6): document APIs

--- Sources/Library/Spawner.swift
+++ Sources/Library/Spawner.swift
@@ -1,6 +1,43 @@
 import Foundation
 
+/// Simulates the spawning behavior of a group over time.
 public struct Spawner {
+    /// Runs a spawning simulation, returning the number of fish at the end of the simulation..
+    ///
+    /// Consider the following initial groups where each number `i` represents that it will take
+    /// `i + 1` days for the a given entity to spawn a new entity:
+    ///
+    /// ```
+    /// 3,4,3,1,2
+    /// ```
+    ///
+    /// Assuming a `cycle` of 7 days and that entities spend 2 days in `childhood`, after 10 days,
+    /// we would expect the following state:
+    ///
+    /// ```
+    /// Initial state: 3,4,3,1,2
+    /// After  1 day:  2,3,2,0,1
+    /// After  2 days: 1,2,1,6,0,8
+    /// After  3 days: 0,1,0,5,6,7,8
+    /// After  4 days: 6,0,6,4,5,6,7,8,8
+    /// After  5 days: 5,6,5,3,4,5,6,7,7,8
+    /// After  6 days: 4,5,4,2,3,4,5,6,6,7
+    /// After  7 days: 3,4,3,1,2,3,4,5,5,6
+    /// After  8 days: 2,3,2,0,1,2,3,4,4,5
+    /// After  9 days: 1,2,1,6,0,1,2,3,3,4,8
+    /// After 10 days: 0,1,0,5,6,0,1,2,2,3,7,8
+    /// ```
+    ///
+    /// With the above example, this method will return 12 as the number of rish in the population.
+    ///
+    /// - Parameters:
+    ///   - initialGroups: The initial group of entities as a set of numbers where each number `i`
+    ///     represents that it will take `i + 1` days for the a given entity to spawn a new entity.
+    ///   - days: The days to simulate.
+    ///   - cycle: The number of days between spawns for each entity.
+    ///   - childhood: The number of days that must pass before a newly spawned entity enters the
+    ///     normal spawning cycle.
+    /// - Returns: The number of fish at the end of the simulation.
     public func spawn(
         with initialGroups: [Int], through days: Int, every cycle: Int, plus childhood: Int
     ) -> Int {
